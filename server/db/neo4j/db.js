7738dadb45ae3c17cc0b7ef2d4b4fa19476a3bc8
edgarc0998 <edgarc0998@gmail.com>
2019-11-11 18:21:58 -0600

    env vars

--- server/db/neo4j/db.js
+++ server/db/neo4j/db.js
@@ -1,7 +1,7 @@
 const neo4j = require('neo4j-driver').v1
 const driver = neo4j.driver(
-  'bolt://localhost',
-  neo4j.auth.basic('neo4j', 'fullstack') //set password in Process.ENV
+  process.env.GRAPHENEDB_URL || 'bolt://localhost'
+  // neo4j.auth.basic('neo4j', 'fullstack') //set password in Process.ENV
 )
 const session = driver.session()
 
