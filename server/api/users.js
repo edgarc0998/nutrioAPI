a519431fa85d983f0b3f6fc44983b30b86daa0b1
edgarc0998 <edgarc0998@gmail.com>
2019-11-12 15:40:22 -0600

    fixed API route to correctly update weeks left

--- server/api/users.js
+++ server/api/users.js
@@ -6,6 +6,46 @@ const {
 } = require('../db/postgres/models/index')
 module.exports = router
 
+function getWeeksLeft(statedGoal, goalWeight, currentWeight) {
+  var cals
+  if (statedGoal === 'Gain 1 lb a week') {
+    cals = 500
+  } else if (statedGoal === 'Gain 0.5 lb a week') {
+    cals = 250
+  } else if (statedGoal === 'Maintain current weight') {
+    cals = 0
+  } else if (statedGoal === 'Lose 0.5 lb a week') {
+    cals = 250
+  } else if (statedGoal === 'Lose 1 lb a week') {
+    cals = 500
+  } else if (statedGoal === 'Lose 1.5 lbs a week') {
+    cals = 750
+  } else if (statedGoal === 'Lose 2 lbs a week') {
+    cals = 1000
+  }
+
+  var lbsPerWeek = cals * 7 / 3500
+
+  var weeksLeft = (goalWeight - currentWeight) / lbsPerWeek
+
+  return Math.abs(weeksLeft)
+}
+
+function formatDate(oldDate) {
+  var newDate
+  var year = oldDate.getFullYear().toString()
+  var month = (oldDate.getMonth() + 1).toString()
+  var day = oldDate.getDate().toString()
+  if (month < 10) {
+    month = '0' + month
+  }
+  if (day < 10) {
+    day = '0' + day
+  }
+  newDate = month + '-' + day + '-' + year
+  return newDate
+}
+
 router.get('/', async (req, res, next) => {
   try {
     const me = await Users.findOne({
@@ -42,11 +82,23 @@ router.put('/:id', async (req, res, next) => {
       bodyType: req.body.bodyType
     })
 
+    var newEndDate = new Date()
+
+    let numWeeks = getWeeksLeft(
+      req.body.statedGoal,
+      req.body.startWeight,
+      req.body.endingWeight
+    )
+    newEndDate.setDate(newEndDate.getDate() + numWeeks * 7)
+
+    var newEndDate = formatDate(newEndDate)
+    var newStartDate = formatDate(new Date())
+
     await longTermGoal.update({
       startWeight: req.body.startWeight,
       endingWeight: req.body.endingWeight,
-      // startDate: req.body.startDate,
-      // endDate: req.body.endDate,
+      startDate: newStartDate,
+      endDate: newEndDate,
       statedGoal: req.body.statedGoal
     })
 
